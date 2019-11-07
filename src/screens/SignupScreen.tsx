ae91d42d7319f914fa712f1126b05f5ac7ef9ee5
Dimitar Atanasov <dimitar.atanasov@scalefocus.com>
2019-11-07 18:26:11 +0200

    [feature]login and navigation ,ref types,props

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,10 +1,9 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import {TouchableOpacity} from 'react-native';
 import {NavigationParams, NavigationScreenProp} from 'react-navigation';
 import {NavigationStackOptions} from 'react-navigation-stack';
 import AuthForm from '../components/AuthForm';
 import {Context as AuthContext} from '../context/AuthContext';
-import {ThemeContext} from '../context/ThemeContext';
 import {INavigationRoutes} from '../enums/ENavigation';
 import {ScreenWrapper, TextLink, TextWrapper} from '../styled/MainStyles';
 
@@ -16,11 +15,6 @@ interface INavigation
 
 const SignupScreen: React.FC<INavigation> = (navigation, props) => {
   const {state, signup}: any = useContext(AuthContext);
-  const check = state.token;
-  const themeContext = useContext(ThemeContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  console.log(typeof navigation);
   return (
     <>
       <ScreenWrapper>
@@ -31,27 +25,6 @@ const SignupScreen: React.FC<INavigation> = (navigation, props) => {
           submitButtonText="Sign up"
           name={props.name}
         />
-        {/* <TextWrapper>
-          <Heading>Sign Up</Heading>
-          <GetIcon name={props.name} color={themeContext.icons.main} />
-        </TextWrapper>
-        <Input
-          label="email"
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          autoCorrect={false}></Input>
-        <Input
-          secureTextEntry
-          label="password"
-          value={password}
-          onChangeText={setPassword}
-          autoCapitalize="none"
-          autoCorrect={false}></Input>
-        {state.errorMessage ? (
-          <TextError>{state.errorMessage}</TextError>
-        ) : null}
-        <Button title="Sign Up!" onPress={() => signup({email, password})} /> */}
         <TouchableOpacity
           onPress={() => navigation.navigate(INavigationRoutes.Signin)}>
           <TextWrapper>
